eba1d1d3cfb9061f60921734a7e71d5e15c55e12
panjanatham.t <panjanatham.t@v2solutions.com>
2023-06-22 17:39:31 +0530

    Added the Jwt token service project.

---
+++ JwtTokenService/CoreWCFService2/IJwtToken.cs
@@ -0,0 +1,63 @@
+﻿using CoreWCF;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Runtime.Serialization;
+using System.Security.Claims;
+using System.Text;
+
+namespace CoreWCFService2
+{
+    [ServiceContract]
+    public interface IJwtToken
+    {
+        [OperationContract]
+        string GenerateJwtToken(string username, string password);
+    }
+    
+    public class Service : IJwtToken
+    {       
+        public string GenerateJwtToken(string username, string password)
+        {
+            var mySecret = Program.Configuration.GetSection("Appsettings").GetSection("SecretKey").Value;
+            
+            var mySecurityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(mySecret));
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim("UserName", username),
+                    new Claim("Password", password)
+                }),
+                Expires = DateTime.UtcNow.AddMinutes(1),
+                SigningCredentials = new SigningCredentials(mySecurityKey, SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
