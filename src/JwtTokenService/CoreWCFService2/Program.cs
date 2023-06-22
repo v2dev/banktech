ab2edc89285c53e3ca4c891169a5dc29485b8b0e
panjanatham.t <panjanatham.t@v2solutions.com>
2023-06-22 18:31:16 +0530

    Added the Jwt token service project.

---
+++ src/JwtTokenService/CoreWCFService2/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+using System;
+using Microsoft.AspNetCore.Server.IISIntegration;
+
+public partial class Program
+{
+    private static IConfiguration _configuration;
+
+    public static IConfiguration Configuration => _configuration;
+
+    static void Main(string[] args)
+    {
+        _configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+        var builder = WebApplication.CreateBuilder();
+
+        builder.Services.AddServiceModelServices();
+        builder.Services.AddServiceModelMetadata();
+        builder.Services.AddAuthentication();
+
+
+        var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+               
+        builder.Services.AddSingleton<IServiceBehavior, UseRequestHeadersForMetadataAddressBehavior>();
+       
+        var app = builder.Build();
+
+        app.UseServiceModel(serviceBuilder =>
+        {
+            serviceBuilder.AddService<Service>();
+            //serviceBuilder.AddServiceEndpoint<Service, IJwtToken>(new BasicHttpBinding(BasicHttpSecurityMode.Transport), "/Service.svc");
+            serviceBuilder.AddServiceEndpoint<Service, IJwtToken>(new BasicHttpBinding(),"/Service.svc");
+            var serviceMetadataBehavior = app.Services.GetRequiredService<ServiceMetadataBehavior>();
+            serviceMetadataBehavior.HttpGetEnabled = true;
+        });       
+
+        app.Run();
+
+    }
+}
